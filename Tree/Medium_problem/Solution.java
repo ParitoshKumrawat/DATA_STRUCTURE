193d21997d9b0b5e4b35b8b0202bebb897ec4928
Paritosh Kumrawat <166283576+ParitoshKumrawat@users.noreply.github.com>
2026-07-12 11:22:24 +0530

    new files

---
+++ Tree/Medium_problem/Solution.java
@@ -0,0 +1,26 @@
+package Tree.Medium_problem;
+import java.util.*;
+
+class Node{
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val){
+        this.val = val;
+        left = null;
+        right =null;
+    }
+}
+
+public class Solution {
+    int maxHeight(Node root){
+        if(root == null)return 0;
+        int max = 0;
+
+        Queue<Node> q = new LinkedList<>();
+
+
+        return max;
+    }
+}
