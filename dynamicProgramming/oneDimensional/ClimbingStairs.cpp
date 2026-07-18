f868072a484f718f0d9910a33d3883c283ee5c7f
Paritosh Kumrawat <166283576+ParitoshKumrawat@users.noreply.github.com>
2026-07-18 20:52:39 +0530

    new file uploaded

--- dynamicProgramming/oneDimensional/ClimbingStairs.cpp
+++ dynamicProgramming/oneDimensional/ClimbingStairs.cpp
@@ -1,9 +1,24 @@
-#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-class ClimbingStairs {
+class ClimbingStairs{
 
 };
 
 int main (){
-  std::cout << "Hello World"<<std::endl;
+ int n = 3;
+
+ vector<int> dp(n+1, -1);
+
+ dp[0] = 1;
+ dp[1] = 1;
+
+ for(int i = 2 ; i<=n ; i++){
+  dp[i] = dp[i-1] + dp[i-2];
+ }
+
+ cout << dp[n];
+
+ return 0;
+
 }
